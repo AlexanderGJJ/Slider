ec1492b43770bf14618639183880c934d41870ba
mikhotin <ad.mihotin@gmail.com>
2021-04-04 00:37:44 +0300

    fix normalizeData and add groups for random choice

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const port = 3001; // || process.env
+const vkGroupsColl = require('../groups.json');
+const getRandom = require('../utils/getRandom');
+
+const port = 3001;
 const token = '1e40884cd5e33f7f7b165ee83bfcf821cca122b41217692ff89f964fd77f11828a4557bba68cd4fceb284';
+const vkGroups = Object.values(vkGroupsColl);
+const vkGroup = vkGroups[getRandom(0, vkGroups.length - 1)];
+console.log(vkGroup);
+
+app.get('/api/get.images', (req, res) => {
+  axios.get(`https://api.vk.com/method/wall.get?domain=${vkGroup}&count=15&filter=owner&access_token=${token}&v=5.52`)
+    .then((response) => res.json(response.data))
+    .catch((error) => res.send(error));
+});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
-app.get('/api/get.images', (req, res) => {
-  axios.get(`https://api.vk.com/method/wall.get?domain=aboutlondon&count=10&filter=owner&access_token=${token}&v=5.52`)
-    .then((response) => res.json(response.data))
-    .catch((error) => res.send(error));
 });
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
--- src/utils/normalizeData.js
+++ src/utils/normalizeData.js
@@ -1,5 +1,8 @@
 const normalizeData = (rawData) => {
   const filteredItems = rawData.reduce((acc, item) => {
+    if (!item.attachments) {
+      return acc;
+    }
     const { attachments } = item;
     const photos = attachments.filter((el) => el.type === 'photo');
     return photos.length > 0 ? [...acc, photos] : acc;
