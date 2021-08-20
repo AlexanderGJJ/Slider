5ef8fc682ccfd598a1c2cb202795bea6e9c42250
mikhotin <ad.mihotin@gmail.com>
2021-08-20 18:53:32 +0300

    update token

--- src/server/server.js
+++ src/server/server.js
@@ -5,20 +5,20 @@ const vkGroupsColl = require('../groups.json');
 const getRandom = require('../utils/getRandom');
 
 const port = 3001;
-const token = '1e40884cd5e33f7f7b165ee83bfcf821cca122b41217692ff89f964fd77f11828a4557bba68cd4fceb284';
+const token = '1712f0859442e7a0e8ee242e0fa5c6da801d0d5daff4318143ef9445064f7d0d872ae874b1fc39d55aad2';
 const vkGroups = Object.values(vkGroupsColl);
 const vkGroup = vkGroups[getRandom(0, vkGroups.length - 1)];
-
-app.get('/api/get.images', (req, res) => {
-  axios.get(`https://api.vk.com/method/wall.get?domain=${vkGroup}&count=15&filter=owner&access_token=${token}&v=5.52`)
-    .then((response) => res.json(response.data))
-    .catch((error) => res.send(error));
-});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+app.get('/api/get.images', (req, res) => {
+  axios.get(`https://api.vk.com/method/wall.get?domain=${vkGroup}&count=15&filter=owner&access_token=${token}&v=5.52`)
+    .then((response) => res.json(response.data))
+    .catch((error) => res.send(error));
 });
 
 app.listen(port, () => console.log(`Port :${port}`));
